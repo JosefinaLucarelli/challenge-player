fe37366567b867804072613c58324acb79375ad0
JosefinaLucarelli <joulucarelli@gmail.com>
2024-10-15 12:16:19 -0300

    add backend url and your logic

--- src/contentManager.js
+++ src/contentManager.js
@@ -1,43 +1,36 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-const db = require('./db');
+const db = require('./db'); // Si tienes NeDB aquí
 
+async function fetchAdsFromAPI() {
+    try {
+        const response = await axios.get('http://localhost:3001/ads'); // URL de Mockoon
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching ads:', error);
+        return [];
+    }
+}
 
 function downloadFile(url, filename) {
-  const writer = fs.createWriteStream(filename);
-  return axios({
-    url,
-    method: 'GET',
-    responseType: 'stream',
-  }).then((response) => {
-    response.data.pipe(writer);
-    return new Promise((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
+    const writer = fs.createWriteStream(filename);
+    return axios({
+        url,
+        method: 'GET',
+        responseType: 'stream',
+    }).then((response) => {
+        response.data.pipe(writer);
+        return new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        });
     });
-  });
-}
-
-
-function fetchAds() {
-  return axios.get('https://api.mock.com/ads').then((response) => {
-    const ads = response.data;
-    ads.forEach((ad) => db.saveAd(ad));
-  });
-}
-
-
-function generateAdContent(ad) {
-  if (ad.type === 'image') {
-    return `<img src="assets/${ad.url}" style="width:100%; height:100%; object-fit: ${ad.fill_screen ? 'fill' : 'contain'};" />`;
-  } else if (ad.type === 'video') {
-    return `<video src="assets/${ad.url}" autoplay loop style="width:100%; height:100%; object-fit: ${ad.fill_screen ? 'fill' : 'contain'};" />`;
-  }
 }
 
 module.exports = {
-  downloadFile,
-  fetchAds,
-  generateAdContent,
+    fetchAdsFromAPI,
+    downloadFile,
 };
+
+
